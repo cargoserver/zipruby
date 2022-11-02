d6379b70d8d27441e8a130a6e484a49afb420594
Dr. Zarkov <zarkov@cargoserver.ch>
2022-11-02 14:07:27 +0100

    Update extconf.rb

--- ext/zipruby/extconf.rb
+++ ext/zipruby/extconf.rb
@@ -4,14 +4,15 @@ if have_header('zlib.h') and have_library('z')
   have_func('fseeko')
   have_func('ftello')
   have_func('mkstemp')
-  create_makefile('zipruby')
-end
-
-cc_version = `#{RbConfig::expand("$(CC) --version".dup)}`
-if cc_version.match?(/clang/i)
-  unless RbConfig::CONFIG['DLDFLAGS'].to_s.include?("dynamic_lookup")
-    # Ref: https://github.com/redis-rb/redis-client/issues/58
-    # Ref: https://bugs.ruby-lang.org/issues/19005
-    $DLDFLAGS << ' -Wl,-undefined,dynamic_lookup '
+  
+  cc_version = `#{RbConfig::expand("$(CC) --version".dup)}`
+  if cc_version.match?(/clang/i)
+    unless RbConfig::CONFIG['DLDFLAGS'].to_s.include?("dynamic_lookup")
+      # Ref: https://github.com/redis-rb/redis-client/issues/58
+      # Ref: https://bugs.ruby-lang.org/issues/19005
+      $DLDFLAGS << ' -Wl,-undefined,dynamic_lookup '
+    end
   end
+  
+  create_makefile('zipruby')
 end
