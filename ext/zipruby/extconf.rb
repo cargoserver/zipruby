d1e9cc8eb2ebce455e741f960b2a14e9af745869
Dr. Zarkov <zarkov@cargoserver.ch>
2022-11-02 14:04:14 +0100

    Fix extconf.rb for clang

--- ext/zipruby/extconf.rb
+++ ext/zipruby/extconf.rb
@@ -6,3 +6,12 @@ if have_header('zlib.h') and have_library('z')
   have_func('mkstemp')
   create_makefile('zipruby')
 end
+
+cc_version = `#{RbConfig::expand("$(CC) --version".dup)}`
+if cc_version.match?(/clang/i)
+  unless RbConfig::CONFIG['DLDFLAGS'].to_s.include?("dynamic_lookup")
+    # Ref: https://github.com/redis-rb/redis-client/issues/58
+    # Ref: https://bugs.ruby-lang.org/issues/19005
+    $DLDFLAGS << ' -Wl,-undefined,dynamic_lookup '
+  end
+end
